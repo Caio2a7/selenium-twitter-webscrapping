30e3700a4b4d421e3d7fe1a0c95c9569be27565b
Caio Daniel <127650414+Caio2a7@users.noreply.github.com>
2023-09-18 12:12:13 -0300

    Giving some comments

--- run.py
+++ run.py
@@ -6,17 +6,24 @@ from email_sender import email_smtp
 import json
 with open("user_data.json", 'r') as json_file:
     data = json.load(json_file)
+
+#Setuping the driver with the browser used, twitter email, password and username
 Setup = setup_driver.DriverSetup(data['browser_name'], data['email'], data['password'],
                                  data['username'])
 Driver = Setup.driver_set_browser(Setup.set_up_options())
 Setup.driver_logging(Driver)
+
+# Web scrapping 
 Scrap = scraper.TwitterScraper(50, 'ele', 'Livro')
 search_scrap = Scrap.search_querys(Driver)
+# The execution of a regex function to filter the data scraped
 Regex = regex_scrap.ScrapRegex(50, 'ele', 'Livro')
 timeline_regex = Regex.scrap_filter(Scrap.scrap_tweets_timeline(Driver))
 Regex.organize_scrap_infos(timeline_regex)
+# Creating the spreadsheet file
 excel = xlsx_maker.SpreadSheet(Regex.organize_scrap_infos(timeline_regex))
 excel.create_excel()
+# Email sending
 email = email_smtp.EmailSender(data['client_email'])
 email.email_usuario()
 email.send_email()
